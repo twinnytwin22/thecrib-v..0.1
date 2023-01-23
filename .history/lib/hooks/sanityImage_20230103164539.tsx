320dd0ffaf5f1608ab81cf601126488b91386dad
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-01-23 16:31:49 -0700

    push 5

--- .history/lib/hooks/sanityImage_20230103164539.tsx
+++ .history/lib/hooks/sanityImage_20230103164539.tsx
@@ -5,7 +5,7 @@ import { createClient } from "next-sanity";
 export const client = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    apiVersion: '2021-10-21',
+    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
     useCdn: true,
     
   })
