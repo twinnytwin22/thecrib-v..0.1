2d4c91c840fcbde14e44779bcf60ea493b2b21bd
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-01-24 16:35:32 -0700

    Styling

--- ui/HeroBanners/Home3DCTABanner.tsx
+++ ui/HeroBanners/Home3DCTABanner.tsx
@@ -18,7 +18,7 @@ import CribScene from 'ui/Models/ThreeDCrib';
                   <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
               </a>
           </div>
-          <div className="lg:mt-0 lg:min-w-96 lg:h-96 md:min-w-36 md:h-36 sm:mt-8">
+          <div className="lg:mt-0 lg:min-w-96 lg:h-96 md:min-w-36 md:h-36 sm:mt-8 order-first sm:order-first lg:order-last md:order-last">
                 <CribScene/>
           </div>                
       </div>
