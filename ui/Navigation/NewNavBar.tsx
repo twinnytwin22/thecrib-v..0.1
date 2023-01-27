7e8d7c409fc06803e6c8df87d4632a923fec8d71
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-01-27 02:24:05 -0700

    2

--- ui/Navigation/NewNavBar.tsx
+++ ui/Navigation/NewNavBar.tsx
@@ -23,7 +23,7 @@ const NewNavBar: NextPage = () => {
 
 
  return (
-    <header>
+    <header className='bg-black'>
       <nav className="bg-black border-gray-200 mx-5 md:mx-auto lg:px-6 py-2.5 dark:bg-black">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="/" className="flex items-center">
