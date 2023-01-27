717021aaea088fb6ec9eed09e440be405fa840f4
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-01-27 16:07:09 -0700

    Update page.tsx

--- app/collectors/page.tsx
+++ app/collectors/page.tsx
@@ -10,8 +10,7 @@ const collectors = await getOwnersForEthCollection(contract)
 console.log(collectors)
 
   return (
-    <div>
-    <div className="bg-white dark:bg-gray-900" >
+    <div className="flex w-screen flex-col items-center bg-white dark:bg-gray-900">
     <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
         <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
             <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">🔥</span> <span className="text-sm font-medium">Not a collector yet? Let's make it happen!</span> 
@@ -31,7 +30,6 @@ console.log(collectors)
 
 
       
-    </div>
   )
 }
 
