8482cf7ec78334ba1d0d88c0902be82b9415d767
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-02-03 19:07:58 -0700

    remove claim

--- ui/Navigation/NewNavBar.tsx
+++ ui/Navigation/NewNavBar.tsx
@@ -49,7 +49,7 @@ const NewNavBar: NextPage = () => {
                         <a href="/explore" className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Explore</a>
                     </li>
                     <li>
-                        <a href="/collectors" className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Collectors</a>
+                        <a href="/collectors"  className="block disabled py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Collectors</a>
                     </li>
          
                 </ul>
--- ui/Sections/CollectionMinter.tsx
+++ ui/Sections/CollectionMinter.tsx
@@ -159,14 +159,18 @@ return (
 
 
 
-                        </div><div className='flex flex-row items-center justify-center items-center space-between'>
+                        </div>
+                       
+                        <div className='flex flex-row items-center justify-center items-center space-between'>
                                 <button className="text-white bg-red-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
                                     onClick={currentCollection.title == "Twinesis" ? handleTwinesisMint : handleMint}>MINT</button>
-                                <button className="text-white bg-red-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                            {currentCollection.slug.current === "super-twinny-sabet" ? (     <button className="text-white bg-red-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
 
-                                    onClick={handleClaim}>CLAIM</button>
+                                    onClick={handleClaim}>CLAIM</button>) : ( "")}
 
-                            </div></>         
+                            </div>
+                            
+                            </>         
     ) : (
         <div className='flex content-center items-center justify-center pt-10' >
     {mintStatus ? <p className='text-white'>Please sign-in to collect</p> : ""}
