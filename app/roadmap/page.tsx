3e2a5c592431e8f4e2cfe082534184a9f1264355
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-02-07 00:47:41 -0700

    Update page.tsx

--- app/roadmap/page.tsx
+++ app/roadmap/page.tsx
@@ -1,14 +1,3 @@
 import { getRoadmap } from 'lib/hooks/roadmapLogic'
 import React from 'react'
 import TestingPanel from 'ui/Testing/TestingPanel'
-
-async function RoadMap() {
-    const roadMap = getRoadmap()
-    if (roadMap) {
-    console.log(roadMap)}
-  return (
-    <TestingPanel props={roadMap}/>
-  )
-}
-
-export default RoadMap
