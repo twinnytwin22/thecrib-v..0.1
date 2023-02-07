cc18df2a8ed2302a52bbf1c99adf7e052236a371
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-02-07 00:48:26 -0700

    Update page.tsx

--- app/roadmap/page.tsx
+++ app/roadmap/page.tsx
@@ -1,3 +1,15 @@
+import { getAllCollections } from 'lib/hooks/get-collection-data'
 import { getRoadmap } from 'lib/hooks/roadmapLogic'
 import React from 'react'
 import TestingPanel from 'ui/Testing/TestingPanel'
+
+async function RoadMap() {
+    const roadMap = getAllCollections()
+    if (roadMap) {
+    console.log(roadMap)}
+  return (
+    <TestingPanel props={roadMap}/>
+  )
+}
+
+export default RoadMap
