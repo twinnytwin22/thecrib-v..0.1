e5e9d6c0a12958717851fb9b31733a97f34d97bc
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-02-08 18:21:10 -0700

    2

--- app/head.tsx
+++ app/head.tsx
@@ -6,7 +6,7 @@ export default function Head() {
       <title>THE CRIB</title>
     
       <Script src="/scripts/segmentAnalytics.js"/>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X216N1DREV"/>
+      <Script  src="https://www.googletagmanager.com/gtag/js?id=G-MPKDV2LKSW"/>
       <Script src="/scripts/ga.js"/>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
--- public/scripts/gtag.js
+++ public/scripts/gtag.js
@@ -2,4 +2,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-X216N1DREV');
+gtag('config', 'G-MPKDV2LKSW');
