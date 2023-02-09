c650a09231251eb698f62dc966ed61ca7e0857e6
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-02-08 17:40:59 -0700

    testing segmient

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,13 @@
+import Script from "next/script"
+import SAnaltyics from "../lib/scripts/segmentAnalytics"
 export default function Head() {
   return (
     <>
       <title>THE CRIB</title>
+      <Script src={SAnaltyics}/>
+ 
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
-      
     </>
   )
 }
