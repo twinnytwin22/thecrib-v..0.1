4313db66d834350920c541af456abea8a0a3fd28
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-02-08 18:09:46 -0700

    adding Google Analytics

--- app/head.tsx
+++ app/head.tsx
@@ -6,6 +6,8 @@ export default function Head() {
       <title>THE CRIB</title>
     
       <Script src="/scripts/segmentAnalytics.js"/>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X216N1DREV"/>
+      <Script src="/scripts/ga.js"/>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
