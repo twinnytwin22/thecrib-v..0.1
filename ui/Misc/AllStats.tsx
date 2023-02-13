1dad782719be6caba441bfc4806bb834a75437f8
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-02-13 02:43:37 -0700

    Update AllStats.tsx

--- ui/Misc/AllStats.tsx
+++ ui/Misc/AllStats.tsx
@@ -11,11 +11,11 @@ function AllStats(collections: any) {
     </div>
     <div className="block max-w-xs md:max-w-sm p-3 md:p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h5 className="mb-2 text-sm md:text-md font-bold tracking-tight text-gray-900 dark:text-white">Collectors</h5>
-            <p className="font-normal text-sm md:text-md text-gray-700 dark:text-gray-400">105</p>
+            <p className="font-normal text-sm md:text-md text-gray-700 dark:text-gray-400">121</p>
     </div>
     <div className="block max-w-xs md:max-w-sm p-3 md:p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h5 className="mb-2 text-sm md:text-md font-bold tracking-tight text-gray-900 dark:text-white">Collectibles</h5>
-            <p className="font-normal text-sm md:text-md text-gray-700 dark:text-gray-400">140</p>
+            <p className="font-normal text-sm md:text-md text-gray-700 dark:text-gray-400">152</p>
     </div>
     </div>
   )
