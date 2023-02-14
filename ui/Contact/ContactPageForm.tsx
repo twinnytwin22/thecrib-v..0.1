ec9f30de15f4a8caa91fd2c438b1e3bd06b0935c
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-02-13 21:12:57 -0700

    Contact Page update

--- ui/Contact/ContactPageForm.tsx
+++ ui/Contact/ContactPageForm.tsx
@@ -26,7 +26,7 @@ const ContactForm = (props: any) => {
         body: JSON.stringify(formData), })
       setStatus('success') 
       if (res) {
-      toast("Your message was sent succesffully")}
+      toast("Your message was sent successfully")}
       setFormData({ email: '', subject: '', message: '' })
     } catch (err) {
       setStatus('error')
@@ -40,11 +40,11 @@ const ContactForm = (props: any) => {
     <section className="bg-white dark:bg-gray-900">
   <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
       <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-      <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+      <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our pitch deck? Looking to collaborate? Let us know.</p>
       <form onSubmit={handleSubmit} className="w-full">
       <div>
         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-         <input className='shadow-sm bg-gray-50 border mb-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required'
+         <input className='shadow-sm bg-gray-50 border mb-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-300 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-primary-500 dark:shadow-sm-light required'
           type="email"
           name="email"
           value={formData.email}
@@ -55,7 +55,7 @@ const ContactForm = (props: any) => {
       <br />
       <div>
               <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-              <input className='shadow-sm bg-gray-50 border mb-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required'
+              <input className='shadow-sm bg-gray-50 border mb-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-300 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-primary-500 dark:shadow-sm-light required'
           type="text"
           name="subject"
           value={formData.subject}
@@ -66,7 +66,7 @@ const ContactForm = (props: any) => {
       <br />
       <div className="sm:col-span-2">
               <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-              <textarea className='shadow-sm bg-gray-50 border mb-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required'
+              <textarea className='shadow-sm bg-gray-50 border mb-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-300 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-primary-500 dark:shadow-sm-light required'
           name="message"
           value={formData.message}
           onChange={handleChange}
