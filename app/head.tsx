e5ca1db648046bed4c14b2aead7793209f95b272
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-03-09 15:21:09 -0700

    Adding metapixel

--- app/head.tsx
+++ app/head.tsx
@@ -4,12 +4,12 @@ export default function Head() {
   return (
     <>
       <title>THE CRIB</title>
-    
+      <Script src="/scripts/metaPixel.js"/>
       <Script src="/scripts/segmentAnalytics.js"/>
       <Script  src="https://www.googletagmanager.com/gtag/js?id=G-MPKDV2LKSW"/>
       <Script src="/scripts/gtag.js"/>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/assets/crib-fav.png" />
     </>
   )
 }
