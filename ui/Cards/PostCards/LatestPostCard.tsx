de2c998e2ac69f6fa8e3e1d66dea29e5291814db
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-03-20 23:09:27 -0700

    Update LatestPostCard.tsx

--- ui/Cards/PostCards/LatestPostCard.tsx
+++ ui/Cards/PostCards/LatestPostCard.tsx
@@ -17,7 +17,7 @@ function LatestDropCard({ image, url, mintPrice }: any) {
             <Link href={url}>
             <button
               type="button"
-              className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
+              className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2  dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
             >
              View Drop
             </button></Link>
