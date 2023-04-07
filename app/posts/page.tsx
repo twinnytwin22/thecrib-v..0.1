d087672cbca5e1e2283ae0ddf3b9436c504f78d9
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-04-07 01:41:31 -0700

    Update page.tsx

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import PostCard from "ui/Cards/PostCards/PostCard";
-import LatestPost from "ui/HeroBanners/LatestPostHero";
 
 async function AllPosts() {
   return (
     <div>
       <section className="bg-white dark:bg-gray-900">
-        <LatestPost />
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
           <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
             <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
