57185006e267fdb13ab744ba49696c6b7691d00e
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-04-11 18:06:32 -0700

    Update CollectionMinter.tsx

--- ui/Sections/CollectionMinter.tsx
+++ ui/Sections/CollectionMinter.tsx
@@ -59,7 +59,7 @@ function CollectionMinter({ collection, data }: any) {
     try { 
       const sdk = new DecentSDK(chainId, signer);
       const decentNFT = await edition.getContract(sdk, contractAddress);
-      const allowListItem = allowlist.find((item) => item.cribAddress as any === session.data?.address);
+      const allowListItem = allowlist.find((item) => item.cribAddress as any === address!);
       const mintPrice = allowListItem ? allowListItem?.price : price;      
       console.log('mintprice:', mintPrice, 'al', allowlist, allowListItem, session)
       const response = await decentNFT.mint(address, BigNumber.from(mintAmount), {
